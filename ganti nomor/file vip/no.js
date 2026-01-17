2605f50bfd4838ea008b8774f24b381167c1796f
gamesspeedshop <muhamadtama619@gmail.com>
2026-01-17 18:59:24 +0700

    Update no.js

--- ganti nomor/file vip/no.js
+++ ganti nomor/file vip/no.js
@@ -25,7 +25,7 @@
             return;
         }
 
-        const phone = '6283896561880'; // ganti nomor WA tujuan
+        const phone = '6285802839214'; // ganti nomor WA tujuan
         const message = `Halo, saya ingin membeli ${product} seharga ${formatRupiah(price)}`;
         const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
         window.open(url, '_blank');
