9f0656ddb46715be0a0fa4c4eabf42ddccd81467
gamesspeedshop <muhamadtama619@gmail.com>
2026-01-18 05:50:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 // 1. Setting Nomor
-const CONFIG_NOMOR_ORDER   = "6285769732492"; 
-const CONFIG_NOMOR_BANTUAN = "6285769732492";
+const CONFIG_NOMOR_ORDER   = ""; 
+const CONFIG_NOMOR_BANTUAN = "";
 const PESAN_BANTUAN        = "Halo Admin, saya butuh bantuan mengenai pembayaran di GSS TEAM.";
 
 // 2. Fungsi Konfirmasi (Pastikan nama fungsi sama persis)
@@ -20,4 +20,5 @@ document.addEventListener("DOMContentLoaded", function() {
             window.open(urlBantuan, '_blank');
         };
     }
+
 });
